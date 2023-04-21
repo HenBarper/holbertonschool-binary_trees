89da183a15b053e0fe7e39efad215456cfccfc22
HenBarper <Benjaminitonio@gmail.com>
2023-04-21 12:23:52 -0700

    1 v5

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,6 +18,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);*/
 
 	node = binary_tree_node(parent, value);
+	if (node = NULL)
+		return (NULL);
 
 	/*node->n = value;*/
 	/*node->parent = parent;*/
