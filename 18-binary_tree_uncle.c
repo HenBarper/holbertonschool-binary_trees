3fa1053ea5ed51bda50f37dde533c4867f7cceb2
HenBarper <Benjaminitonio@gmail.com>
2023-04-24 15:19:55 -0700

    18 v1

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
+/**
+ * binary_tree_uncle - finds uncle of node
+ * @node: node to find sibling of
+ * Return: uncle node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	return (binary_tree_sibling(node->parent);
+}
+
+/**
+ * binary_tree_sibling - finds sibling node
+ * @node: node to find sibling of
+ * Return: sibling node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if ((node->parent)->left == node)
+		return ((node->parent)->right);
+	else
+		return ((node->parent)->left);
+}
